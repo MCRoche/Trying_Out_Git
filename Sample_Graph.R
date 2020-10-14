88af58b88523b01cb2503c7aac76f400facf188f
MCRoche <maryclare.roche@gmail.com>
2020-10-14 10:41:22 -0500

    Added new file to repository

---
+++ Sample_Graph.R
@@ -0,0 +1,19 @@
+##----
+##
+##  Test File
+##
+##----
+
+#' This file makes a sample graph.
+#' It's point is to demonstrate how to save a file to GitHub and track changes.
+
+library(ggplot2)
+
+# Some points:
+x <- data.frame(a=runif(10),
+                b=runif(10))
+
+# Some graph:
+ggplot(x, aes(a,b)) +
+  geom_point() +
+  xlab("Hello")
