c1c56ef6f2b1412f0beed4e878e45a043a4fa1de
MCRoche <maryclare.roche@gmail.com>
2020-10-15 11:39:31 -0500

    Yet another graph

---
+++ Sample_Graph3.R
@@ -0,0 +1,21 @@
+##----
+##
+##  Test File: Sample_Graph.R
+##
+##----
+
+#' This file makes a sample graph.
+#' It's point is to demonstrate how to save a file to GitHub and track changes.
+
+library(ggplot2)
+
+# Some points:
+x <- data.frame(a=runif(30),
+                b=runif(30))
+
+# Some graph:
+ggplot(x, aes(a,b)) +
+  geom_point() +
+  xlab("Salutaions") +
+  ylab("Up") +
+  ggtitle("Graph Title 3")
